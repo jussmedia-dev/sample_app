244c8e94635978389b75ab3fb3abd51feed6543c
Julian Skinner <julian@jussmedia.com>
2014-02-05 09:02:55 -0500

    Updated RSpec Contact test to use 'have_selector' to be more strict

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -31,7 +31,7 @@ describe  "Static Pages" do
   describe "Contact page" do
     before { visit contact_path }
     
-    it { should have_content('Contact') }
+    it { should have_selector('h1', text: 'Contact') }
     it { should have_title(full_title('Contact Us')) }  
   end
 end
