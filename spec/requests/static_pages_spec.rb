91ffc1809439f45f9e1503420285da3916211d2a
Julian Skinner <julian@jussmedia.com>
2014-02-05 09:09:42 -0500

    Add test to validate the links navigate to correct page.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -34,6 +34,25 @@ describe  "Static Pages" do
     it { should have_selector('h1', text: 'Contact') }
     it { should have_title(full_title('Contact Us')) }  
   end
+  
+  it "should have the right links on the layout" do 
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact Us'))
+    
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+    
+    click_link "sample app"
+    expect(page).to have_title(full_title(''))
+  end
 end
 
 
