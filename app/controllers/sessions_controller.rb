fdf05797a87a0367a3c8e3ba26889d87368f480a
Julian Skinner <julian@jussmedia.com>
2014-02-06 17:44:39 -0500

    Invalid login attempt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,14 @@ class SessionsController < ApplicationController
   end
   
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #Sign the user in and redirect to the user's show page
+    else
+      # Create an error message and re-render the signin form.
+      flash[:error] = 'Invalid email/password combintation' #Not quite right!
+      render 'new'
+    end
   end
   
   def destroy
