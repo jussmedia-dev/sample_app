cbed515084d4231f855c33a45d92cbb4bc9a5071
Julian Skinner <julian@jussmedia.com>
2014-02-20 12:45:33 -0500

    Created user/micropost associations.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,9 @@ describe User do
   it { should respond_to{:password_confirmation} }
   it { should respond_to{:authenticate} }
   it { should respond_to{:admin} }
+ 
+  # Micropost 
+  it { should respond_to(:microposts) }
   
   it { should be_valid }
   it { should_not be_admin }
